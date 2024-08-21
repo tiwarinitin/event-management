1ce87ec46d9f3d81f66c2726b084449f3bd8fe77
Nitin <nitin.tiwari6@outlook.com>
2024-08-21 19:18:50 +0530

    added db packages to work with sqlit3 db

---
+++ db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	DB, err := sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Database connection does not establied!")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createTable()
+}
+
+func createTable() {
+	createEventTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
+	)
+	`
+
+	_, err := DB.Exec(createEventTable)
+
+	if err != nil {
+		panic("could not create db table!")
+	}
+}
