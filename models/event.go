eb456a8b10181609721fec7ee0f9d86382ba6b9d
Nitin <nitin.tiwari6@outlook.com>
2024-08-23 02:31:04 +0530

    added new api to update event by id

--- models/event.go
+++ models/event.go
@@ -86,3 +86,27 @@ func GetAllEvents() ([]Event, error) {
 
 	return events, nil
 }
+
+func (event Event) Update() error {
+	query := `
+	UPDATE events
+	SET name=?, description=?,dateTime=?, user_id=?
+	WHERE id = ?
+	`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(
+		event.Name,
+		event.Description,
+		event.DateTime,
+		event.UserID,
+		event.ID,
+	)
+	return err
+
+}
