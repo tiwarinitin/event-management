5bbd3b81c142306679db778bf87292c0f41c8673
Nitin <nitin.tiwari6@outlook.com>
2024-08-25 14:42:16 +0530

    updated events and routes to use middleware for authentication

--- routes/event.go
+++ routes/event.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"example.com/event-mgmt/models"
-	"example.com/event-mgmt/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -34,28 +33,16 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized!"})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized token"})
-		return
-	}
 
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data!"})
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 	err = event.Save()
 	if err != nil {
